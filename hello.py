3fcf5a78bf298db66710c0268afa8a82f27a071a
Johan Kohler <johan.kohler@gmail.com>
2012-08-10 19:44:06 +0200

    Starting point - flask example code

---
+++ hello.py
@@ -0,0 +1,60 @@
+from flask import Flask, flash, redirect, render_template, \
+     request, url_for, session
+
+app = Flask(__name__)
+
+app.secret_key = 'some_secret'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or \
+                request.form['password'] != 'secret':
+            error = 'Invalid credentials'
+        else: 
+            session['username'] = request.form['username']
+            flash('You were successfully logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+@app.route('/hello')
+def hello():
+    return 'Hello World'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
