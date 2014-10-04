fb4f018d3a6c8dfb8e05238fe705776e317a6bd0
Jeremy Thurgood <firxen@gmail.com>
2014-10-04 12:06:46 +0200

    Clean up test_sharepad.py.

--- sharepad/tests/test_sharepad.py
+++ sharepad/tests/test_sharepad.py
@@ -1,18 +1,16 @@
-from __future__ import with_statement
-from contextlib import closing
-import sqlite3
 from sharepad import sharepad_db
 from sharepad import server
+
 
 def make_pizza():
     sharepad_db.add_pizza(sharepad_db.generate_pizza_by_style('cheesy'))
 
+
 def clear_db():
     pass
+
 
 def test_get_random():
     make_pizza()
     pizza = server.get_random_pizza()
     assert(sharepad_db.is_valid_pizza(pizza))
-
-
